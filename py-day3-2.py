369b45319bbd4192e719f4983f0ac233608efc4a
kumariyandamuri <125617730+kumariyandamuri@users.noreply.github.com>
2023-10-11 19:12:46 +0530

    Add files via upload

---
+++ py-day3-2.py
@@ -0,0 +1,7 @@
+'''assignment1:
+write a program to print biggest of two numbers'''
+num=int(input("Enter number value:"))
+if(num%2==0):
+    print("Given num is Even")
+else:
+    print("Given num is  Odd")
